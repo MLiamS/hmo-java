5af0f5e5051a6a5b89052883b4412af0c5bbd7aa
Liam Stabeno and Ryan McAlpin <liam@>
2017-03-28 10:16:06 -0700

    create initial files, pass instatiation test

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,9 +1,11 @@
 import org.sql2o.*;
 
 public class Doctor {
+  private String name;
+  private int specialityId;
 
-  public Doctor() {
-
+  public Doctor(String name, int sId) {
+    this.name = name;
+    this.specialityId = sId;
   }
-
 }
